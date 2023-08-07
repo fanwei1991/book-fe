86cdaeb2c927042f8ac1991814d740cf91b257e1
fanwei <wei.fan@hcr.com.cn>
2023-08-07 16:10:11 +0800

    chore: build

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ npm run docs:build
 
 cd docs/.vuepress/dist
 
-git push -f https://github.com/fanwei1991/book-fe.git master:gh-pages
+git push -f https://github.com/fanwei1991/book-fe.git main:gh-pages
 
 cd -
